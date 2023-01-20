a3f09a1575115266090904006086ec571abc3715
ShMP <matveyshnytkin@gmail.com>
2023-01-20 12:26:10 +0300

    Add Company and Station update to UI

--- src/front/charging/src/components/CompanyList.vue
+++ src/front/charging/src/components/CompanyList.vue
@@ -15,16 +15,19 @@ const newCompany = ref({
 })
 //companyId to template
 const newStations = ref({})
-const companies = ref([])
+const companies = ref({})
+
+const editedStations = ref(new Set())
+const editedCompanies = ref(new Set())
 
 
 function addCompany() {
   axios.post('http://localhost:8080/company', newCompany.value)
       .then(response => {
         const company = response.data
-        company.stations = []
+        company.stations = {}
 
-        companies.value.push(company)
+        companies.value[company.id] = company
         newStations.value[company.id] = {
           name: "",
           latitude: null,
@@ -42,8 +45,37 @@ function addStation(companyId) {
       .then(response => {
         const station = response.data
 
-        companies.value.find(c => c.id === companyId)
-            .stations.push(station)
+        companies.value[companyId]
+            .stations[station.id] = station
+      })
+      .catch(error => {
+        window.alert(error)
+      })
+}
+
+function updateCompany(id) {
+  axios.put(`http://localhost:8080/company`, companies.value[id])
+      .then(response => {
+        const company = response.data
+        company.stations = companies.value[id].stations
+
+        companies.value[id] = company
+
+        editedCompanies.value.delete(id)
+      })
+      .catch(error => {
+        window.alert(error)
+      })
+}
+
+function updateStation(companyId, id) {
+  axios.put(`http://localhost:8080/station`, companies.value[companyId].stations[id])
+      .then(response => {
+        const station = response.data
+
+        companies.value[companyId].stations[id] = station
+
+        editedStations.value.delete(id)
       })
       .catch(error => {
         window.alert(error)
@@ -53,7 +85,7 @@ function addStation(companyId) {
 function deleteCompany(id) {
   axios.delete(`http://localhost:8080/company/${id}`)
       .then(_ => {
-        companies.value = companies.value.filter(c => c.id !== id)
+        delete companies.value[id]
         delete newStations.value[id]
       })
       .catch(error => {
@@ -64,7 +96,7 @@ function deleteCompany(id) {
 function deleteStation(companyId, stationId) {
   axios.delete(`http://localhost:8080/station/${stationId}`)
       .then(_ => {
-        const company = companies.value.find(c => c.id === companyId)
+        const company = companies.value[companyId]
         company.stations = company.stations.filter(s => s.id !== stationId)
       })
       .catch(error => {
@@ -75,18 +107,46 @@ function deleteStation(companyId, stationId) {
 
 <template>
   <div v-for="{id : companyId, name, parentCompany, stations} in companies">
-    {{ companyId }} {{ name }} {{ parentCompany?.id }}
-    <button @click="deleteCompany(companyId)">
-      Delete Company
-    </button>
-
-    <div class="stations">
-    <div v-for="{id : stationId, latitude, longitude, name} in stations">
-      {{ stationId }} {{ name }} {{ latitude }} {{ longitude }}
-      <button @click="deleteStation(companyId, stationId)">
-        Delete Station
+    <div class="company-entry">
+      <div class="company-data" v-if="!editedCompanies.has(companyId)">
+        {{ companyId }} {{ name }} {{ parentCompany?.id }}
+        <button @click="editedCompanies.add(companyId)">
+          Edit Company
+        </button>
+      </div>
+      <div class="company-data" v-else>
+        <input v-model="companies[companyId].name" placeholder="Name">
+<!--        <input v-model="companies[companyId].parentCompanyId" type="number" placeholder="Parent Id">-->
+        <button @click="updateCompany(companyId)">
+          Submit Company
+        </button>
+      </div>
+      <button @click="deleteCompany(companyId)">
+        Delete Company
       </button>
     </div>
+
+    <div class="stations">
+      <div v-for="{id : stationId, latitude, longitude, name} in stations" class="station-entry">
+        <div class="station-data" v-if="!editedStations.has(stationId)">
+          {{ stationId }} {{ name }} {{ latitude }} {{ longitude }}
+          <button @click="editedStations.add(stationId)">
+            Edit Station
+          </button>
+        </div>
+        <div class="station-data" v-else>
+          <input v-model="companies[companyId].stations[stationId].name" placeholder="Name">
+          <input v-model="companies[companyId].stations[stationId].latitude" type="number" placeholder="Latitude">
+          <input v-model="companies[companyId].stations[stationId].longitude" type="number" placeholder="Longitude">
+          <button @click="editedStations.delete(stationId)">
+            Submit Station
+          </button>
+        </div>
+        <button @click="updateStation(companyId, stationId)">
+          Delete Station
+        </button>
+      </div>
+
       <input v-model="newStations[companyId].name" placeholder="Name">
       <input v-model="newStations[companyId].latitude" type="number" placeholder="Latitude">
       <input v-model="newStations[companyId].longitude" type="number" placeholder="Longitude">
@@ -95,6 +155,7 @@ function deleteStation(companyId, stationId) {
       </button>
     </div>
   </div>
+
   <div>
     <input v-model="newCompany.name" placeholder="Name">
     <input v-model="newCompany.parentCompanyId" type="number" placeholder="Parent Id">
@@ -119,12 +180,20 @@ input {
   margin: 0 0.2rem 0 0.2rem;
 }
 
+button {
+  margin: 0 0.2rem 0 0.2rem;
+}
+
 input[type="number"] {
   width: 5rem;
 }
 
 .stations {
   padding-left: 2rem;
+}
+
+.station-entry, .company-entry {
+  display: flex;
 }
 
 @media (min-width: 1024px) {
